24323c6aeb132d98c529f67c23dcc24ab7bfd2f2
ines <inesbourouissi@gmail.com>
2024-04-05 16:36:03 +0100

    ines lab5

---
+++ src/seleniumScripts/TestGenerator.java
@@ -0,0 +1,66 @@
+package seleniumScripts;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TestGenerator {
+
+    public static WorkingWithChrome connection ;
+
+    @BeforeAll
+    static void beforeAll() {
+    	connection = new WorkingWithChrome();
+    	connection.invokeBrowser();
+    }
+
+	@AfterAll
+	static void afterAll() {
+		connection.closeDriver();
+	}
+	
+	@Test
+	public void TestWorfklow() {
+		this.TestingWebsiteTitle();
+		this.TestingLoginFalsePassword();
+		this.TestingLogin();
+		this.TestingLogout();
+	}
+	
+    public void TestingWebsiteTitle() {
+    	String actualTitle = connection.getTitleforTest();
+    	assertEquals("Azur city Géant Drive : pour vos courses en ligne du quotidien", actualTitle);
+    }
+    
+    public void TestingLoginFalsePassword() {
+    	String email ="inesbourouissi@gmail.com";
+    	String password ="1234ines";
+        connection.login(email, password);
+       	WebElement errorBox = connection.driver.findElement(By.xpath("//*[@id=\"content\"]/section/div/ul/li")); 
+    	assertTrue(errorBox.isDisplayed());
+    }
+    
+    public void TestingLogin() {
+    	String email ="inesbourouissi@gmail.com";
+    	String password ="12345ines";
+        connection.login(email, password);
+        assertEquals("Mon compte", connection.driver.getTitle());
+    }
+    
+    public void TestingLogout() {
+        connection.logout();
+        assertEquals("Identifiant", connection.driver.getTitle());
+    }
+    
+    @Test
+    public void testingSearch() {
+    	connection.search("danette");
+    	WebElement product = connection.driver.findElementByXPath("//*[@id=\"js-product-list\"]/div/div[1]/article/div/div[3]/div[2]/p");
+    	assertEquals("DANETTE",product.getText());
+    }
+	
+}
---
+++ src/seleniumScripts/WorkingWithChrome.java
@@ -0,0 +1,91 @@
+package seleniumScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WorkingWithChrome {
+
+	
+	ChromeDriver driver;
+	
+	String baseUrl = "https://www.geantdrive.tn/azur-city/";
+	
+	public void invokeBrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ines\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.get(baseUrl);
+	}
+	
+
+	public String getTitleforTest() {
+		String pageTitle = driver.getTitle();
+		return pageTitle;
+	}
+
+	public void getTitle() {
+		String pageTitle = driver.getTitle();
+		System.out.println("final result: "+ pageTitle);
+	}
+
+	public void closeDriver() {
+		driver.close();
+	}
+	
+
+    void goToPage(String page){
+        WebElement elem = driver.findElement(By.linkText(page));
+        elem.click();
+    }
+    
+    void login(String username, String password){
+
+        driver.findElement(By.id("_desktop_user_info")).click();
+        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Identifiez-vous']"))).click();
+        
+        // fill out the login form with data
+        driver.findElement(By.name("login")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+		
+		driver.findElement(By.id("submit-login")).click();
+
+    }
+
+    void logout(){
+        driver.findElement(By.id("_desktop_user_info")).click();
+        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Voir mon compte client']"))).click();
+        driver.findElement(By.xpath("//*[@id=\"main\"]/footer/div/a")).click();
+    }
+    
+    public boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        }
+        catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+    void search(String product) {
+    	driver.findElement(By.id("search_toggle")).click();
+    	driver.findElement(By.id("search_query_top")).sendKeys(product);
+    	driver.findElement(By.name("submit_search")).click();	
+    }
+	
+/*	
+	public static void main(String [] args) {
+		
+		WorkingWithChrome connection = new WorkingWithChrome();
+		connection.invokeBrowser();
+		connection.getTitle();
+		connection.closeDriver();
+		
+	}
+*/	
+	
+}
